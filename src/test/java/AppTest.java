cf507844259b2c453a3513d16d76388bde9dccd8
heokyeongju <beewt@naver.com>
2022-07-15 10:41:00 +0900

    repository 생성, ãArrayList class 생성

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    void ArrayList_클래스가_존재한다() {
+        ArrayList al = new ArrayList();
+    }
+}
